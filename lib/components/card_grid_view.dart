fd939caec64e5e4f72c12548a6eb267d0cd63dc7
Joao Gabriel <gabrielriery@gmail.com>
2023-02-10 16:22:53 -0500

    WIP

--- lib/components/card_grid_view.dart
+++ lib/components/card_grid_view.dart
@@ -1,30 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:yugioh_cards/components/display_card.dart';
+import 'package:yugioh_cards/components/card_view.dart';
 import 'package:yugioh_cards/models/models.dart';
 
-class CardGridView extends StatelessWidget {
+class CardGridView extends StatefulWidget {
   final List<YugiohCard> cards;
 
   const CardGridView({super.key, required this.cards});
 
   @override
+  State<CardGridView> createState() => _CardGridViewState();
+}
+
+class _CardGridViewState extends State<CardGridView> {
+  YugiohCard? selectedCard;
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16),
       child: GridView.builder(
-        itemCount: cards.length,
+        itemCount: widget.cards.length,
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 5, childAspectRatio: 3.1 / 4.6),
         itemBuilder: (context, index) {
-          final simpleCard = cards[index];
-          return Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: DisplayCard(card: simpleCard),
+          final simpleCard = widget.cards[index];
+          return GestureDetector(
+            onTap: () {
+              if (simpleCard == selectedCard) {
+                selectedCard = null;
+              } else {
+                selectedCard = simpleCard;
+              }
+
+              setState(() {});
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: CardView(
+                card: simpleCard,
+                isSelected: selectedCard == simpleCard,
+              ),
+            ),
           );
         },
       ),
     );
   }
-
-  
 }
--- lib/components/components.dart
+++ lib/components/components.dart
@@ -1,4 +1,2 @@
 export 'card_grid_view.dart';
-export 'display_card.dart';
-export 'spell_card.dart';
-export 'trap_card.dart';
+export 'card_view.dart';
