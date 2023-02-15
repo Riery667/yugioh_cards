3cec72e5833c6937594fbf457a196bb7ca30a991
Joao Gabriel <gabrielriery@gmail.com>
2023-02-14 19:06:53 -0500

    Upgrade in performance
    MiniCardGridView no longer rebuild all the list, only the selected card.

--- lib/components/mini_card_grid_view.dart
+++ lib/components/mini_card_grid_view.dart
@@ -8,27 +8,28 @@ class MiniCardGridView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<CardManager>(
-      builder: (context, cardManager, child) {
-        return GridView.builder(
-          itemCount: cardManager.cards.length,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 7, childAspectRatio: 3.1 / 4.6),
-          itemBuilder: (context, index) {
-            final simpleCard = cardManager.cards[index];
-            return GestureDetector(
-              onTap: () {
-                cardManager.selectCard(simpleCard);
-              },
-              child: Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: MiniCardView(
+    final cardManager = Provider.of<CardManager>(context, listen: false);
+    return GridView.builder(
+      itemCount: cardManager.cards.length,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 7, childAspectRatio: 3.1 / 4.6),
+      itemBuilder: (context, index) {
+        final simpleCard = cardManager.cards[index];
+        return GestureDetector(
+          onTap: () {
+            cardManager.selectCard(simpleCard);
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Consumer<CardManager>(
+              builder: (context, cardManager, child) {
+                return MiniCardView(
                   card: simpleCard,
                   isSelected: cardManager.selectedCard == simpleCard,
-                ),
-              ),
-            );
-          },
+                );
+              },
+            ),
+          ),
         );
       },
     );
